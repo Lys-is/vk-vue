874b9849cf385cead76850cc8d7a3c97fa21d489
Lys-is <nikneup@yandex.ru>
2025-01-26 12:21:26 +0300

    s

--- src/components/Oauth.vue
+++ src/components/Oauth.vue
@@ -37,10 +37,22 @@ const store = useStore();
                 });
                 return VKID;
             },
-        },
-        created() {
-            let params = Object.fromEntries(new URLSearchParams(location.search));
 
+        },
+        async mounted() {
+            //let params = Object.fromEntries(new URLSearchParams(location.search));
+            let params = {
+                "code": "vk2.a.FtkxDHcIbo-oikXIY_P9VTkNpmvydURPASFCCviiQPQ9K6BxPyW7iIWKxqtojIhsgIg52h6vutFlmAwdisqIWB-i6RU_qLmMUy3j_cnQidtNiig9ufvHNfThe3sUGkJO4Yt9OqMdwxxeaxspXeRLa_DlefAtTezfLTn2KbRg5O9Xp8YyLA4uDtNJ6wRZtR7TszGuuztrHh6VJyFXjeUw3g",
+                "expires_in": "600",
+                "device_id": "VBQFJs3kVTJozlikyjgqfsE4GrGUFDwrFporPKIh1rH-gxbQl7bkAYOk8Jz5ew8r97iHKf8PHc4FDDTyinQw4g",
+                "state": "M8mbC4QHkVkgJGXFLVVXRxfGVeFVTJ8kJX0YRoLHaRcAiBSx",
+                "ext_id": "JYrw0bcCUU99tcM2Yr4Db_LzkpfneeOAoWA7Fd7fVK37_gzAoXRNpHh4i2i-63voxItB6PhTYJpSBhRqAgxTm5pDUuEBfG-iby973h8KJsPCE0qB1Xd1dY50Euv37X5lg7d0vxnur_Czu_8RyDFKI6GyfdF_SjOL3N847i2xcf2xpXc",
+                "type": "code_v2"
+            };
+            if(params.code && params.device_id) {
+                let response = await this.VKID.Auth.exchangeCode(params.code, params.device_id)
+                console.log(response)
+            }
             console.log(params)
         },
     };
